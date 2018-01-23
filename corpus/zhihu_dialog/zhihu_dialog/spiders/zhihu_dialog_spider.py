a0121e932ead20c247e32e0e53b6653398e4bde4
hao-pc <1347261894@qq.com>
2018-01-23 21:08:01 +0800

    模拟登陆

--- corpus/zhihu_dialog/zhihu_dialog/spiders/zhihu_dialog_spider.py
+++ corpus/zhihu_dialog/zhihu_dialog/spiders/zhihu_dialog_spider.py
@@ -1,30 +1,26 @@
 # -*- coding: utf-8 -*-
-
-
-import scrapy
-from zhihu_dialog.items import ZhihuDialogItem
 import json
 import os
+
+import scrapy
 import time
+
 from PIL import Image
 
 
-class AddressSpider(scrapy.Spider):
-    name = 'zhihu_dialog_spider'
+class ZhihuloginSpider(scrapy.Spider):
+    name = 'zhihulogin'
     allowed_domains = ['www.zhihu.com']
     start_urls = ['https://www.zhihu.com/']
-    Agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)' \
-            ' Chrome/50.0.2661.102 Safari/537.36'
+    Agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
     header = {
         'User-Agent': Agent,
     }
 
     def parse(self, response):
-        item = ZhihuDialogItem()
-
-        print('======================================================================')
+        #主页爬取的具体内容
         print(response.body)
-        # 主页爬取的具体内容
+        print('=====================================================================')
         pass
 
     def start_requests(self):
@@ -43,29 +39,33 @@ class AddressSpider(scrapy.Spider):
         except:
             print(u'请到 %s 目录找到captcha.jpg 手动输入' % os.path.abspath('captcha.jpg'))
         captcha = input("please input the captcha\n>")
-        return scrapy.FormRequest(url='https://www.zhihu.com/#signin', headers=self.header, callback=self.login, meta={
-            'captcha': captcha
-        })
+        return scrapy.FormRequest(url='https://www.zhihu.com/#signin', headers=self.header
+                                  , callback=self.login, meta={'captcha': captcha})
 
     def login(self, response):
-        xsrf = response.xpath("//input[@name='_xsrf']/@value").extract_first()
-        if xsrf is None:
-            return ''
+        xsrf = str(response.headers['Set-Cookie']).split(';')[0].split('=')[1]
         post_url = 'https://www.zhihu.com/login/phone_num'
+        # post_url = 'https://www.zhihu.com/api/v3/oauth/sign_in'
         post_data = {
             "_xsrf": xsrf,
-            "phone_num": 'your phone number',
-            "password": 'your password',
+            "phone_num": '18101282413',
+            "password": '00oo00OO',
             "captcha": response.meta['captcha']
         }
-        return [scrapy.FormRequest(url=post_url, formdata=post_data, headers=self.header, callback=self.check_login)]
+        # return [scrapy.FormRequest(url=post_url, formdata=post_data,
+        #                            headers=self.header, callback=self.check_login)]
+        yield scrapy.FormRequest(url=post_url, formdata=post_data,
+                                   headers=self.header, callback=self.check_login)
 
     # 验证返回是否成功
     def check_login(self, response):
         js = json.loads(response.text)
         if 'msg' in js and js['msg'] == '登录成功':
             for url in self.start_urls:
-                return [scrapy.FormRequest(url=post_url, formdata=post_data, headers=self.header,
-                                           callback=self.check_login)]
+                yield scrapy.Request(url=url, headers=self.header, dont_filter=True
+                                     , callback=self.parse)
 
-                # yield scrapy.Request(url=url, headers=self.header, dont_filter=True)
+
+
+
+
