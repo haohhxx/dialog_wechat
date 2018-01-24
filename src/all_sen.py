8afb91507e87a3698180be0b77793c905c5b54ed
haozhenyuan@peopleyun.cn <haozhenyuan@peopleyun.cn>
2018-01-24 17:32:06 +0800

    attention

---
+++ src/all_sen.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+
+import tensorflow as tf
+from tensorflow.python.layers import core as layers_core
+from tensorflow.contrib.rnn import LSTMCell, LSTMStateTuple, GRUCell
+from tensorflow.contrib.layers import xavier_initializer
+from tensorflow.contrib.seq2seq import AttentionWrapper, AttentionWrapperState, \
+    BasicDecoder, BeamSearchDecoder, dynamic_decode, \
+    TrainingHelper, sequence_loss, tile_batch, \
+    BahdanauAttention, LuongAttention
+
+
+class Utterance2Seq(object):
+
+    def _build_utterance_attention(self, sen_attention):
+        with tf.variable_scope('utterance_attention'):
+            attention_mechanism = BahdanauAttention(
+                self.attention_num_units,
+                encoder_outputs,
+                encoder_lengths,
+                name="attention_fn"
+            )
+            decoder_cell = AttentionWrapper(
+                self.decoder_cell,
+                attention_mechanism,
+                attention_layer_size=self.attention_depth,
+                output_attention=True,
+            )
+            decoder_initial_state = decoder_cell.zero_state(self.batch_size, tf.float32)\
+                                                .clone(cell_state=encoder_final_state)
+        return decoder_initial_state
--- src/sen_seq.py
+++ src/sen_seq.py
@@ -31,10 +31,11 @@ class Sen2Seq(object):
     EOS = 1
 
     def __init__(self):
+        self.decoder_cell = GRUCell(self.decoder_rnn_state_size)
         pass
 
     def _build_encoder(self, encoder_inputs, word_embedding, encoder_lengths):
-        with tf.variable_scope('word_embedding'):
+        # with tf.variable_scope('word_embedding'):
             # word_embedding = tf.get_variable(
             #     name="word_embedding",
             #     shape=(num_word, embedding_dim),
@@ -42,7 +43,7 @@ class Sen2Seq(object):
             #     dtype=tf.float32
             # )
             # batch_size, max_time, embed_dims
-            encoder_input_vectors = tf.nn.embedding_lookup(word_embedding, encoder_inputs)
+        encoder_input_vectors = tf.nn.embedding_lookup(word_embedding, encoder_inputs)
         with tf.variable_scope('encoder'):
             encoder_cell = LSTMCell(self.encoder_rnn_state_size)
             encoder_outputs, encoder_final_state = tf.nn.dynamic_rnn(
@@ -53,23 +54,25 @@ class Sen2Seq(object):
                 dtype=tf.float32
             )
             encoder_final_state.set_shape([self.batch_size, self.encoder_rnn_state_size])
+        return encoder_outputs, encoder_final_state
 
-    def _word_level_attention(self, encoder_outputs, word_embedding, encoder_lengths):
-        with tf.variable_scope('attention'):
+    def _word_level_attention(self, encoder_outputs, encoder_final_state, encoder_lengths):
+        with tf.variable_scope('word_attention'):
             attention_mechanism = BahdanauAttention(
                 self.attention_num_units,
                 encoder_outputs,
                 encoder_lengths,
                 name="attention_fn"
             )
-            beam_attention_mechanism = BahdanauAttention(
-                attention_num_units,
-                tiled_encoder_outputs,
-                tiled_encoder_lengths,
-                name="attention_fn"
+            decoder_cell = AttentionWrapper(
+                self.decoder_cell,
+                attention_mechanism,
+                attention_layer_size=self.attention_depth,
+                output_attention=True,
             )
-
-
+            decoder_initial_state = decoder_cell.zero_state(self.batch_size, tf.float32)\
+                                                .clone(cell_state=encoder_final_state)
+        return decoder_initial_state
 
 
 
