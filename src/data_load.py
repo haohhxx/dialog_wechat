8f1c7b5ef4ce9349f41ee29c29d12bb059cad43b
hhxx2015 <zhenyuan_hao@163.com>
2018-01-30 21:34:42 +0800

    todo

--- src/data_load.py
+++ src/data_load.py
@@ -11,8 +11,8 @@ def load_w2vec(vec_bin_path):
     word2id = {}
     id2word = {}
     for i, j in enumerate(w2v.vocab):
-        word2id[i] = j
-        id2word[j] = i
+        id2word[i] = j
+        word2id[j] = i
     return word2id, id2word, vectors
 
 
@@ -20,6 +20,9 @@ class DataLoader(object):
 
     iternub = 1
     max_sentence_length = 0
+    datas = []
+    lines = []
+    train_batch_index = 0
 
     def __init__(self, batch_size=30
                  , content_path=r'./data/content_cut'
@@ -27,16 +30,15 @@ class DataLoader(object):
                  , max_iter=50):
 
         self.word_to_id, self.id_to_word, self.vectors = load_w2vec(vec_bin_path)
-        self.datas = []
-        self.lines = []
+
 
         self.max_iter = max_iter
         self.batch_size = batch_size
         self.content_path = content_path
         self.statistic_voc()
-        self.text_array = np.zeros([len(self.lines), self.max_sentence_length], dtype=np.int32)
+        self.x_array = np.zeros([len(self.lines), self.max_sentence_length], dtype=np.int32)
+        self.y_array = np.zeros([len(self.lines), self.max_sentence_length], dtype=np.int32)
         self.train_test_index = int(len(self.lines)/2)
-        self.train_batch_index = 0
         self.test_batch_index = self.train_test_index
         self.load()
 
@@ -44,34 +46,38 @@ class DataLoader(object):
         with open(self.content_path, 'r', encoding='utf-8') as f:
             self.lines = f.readlines()
             for line in self.lines:
-                line = line.split('\t')[1]
-                words = [word.strip() for word in line.split(' ')]
+                words = line.split(' ')
                 if self.max_sentence_length < len(words):
                     self.max_sentence_length = len(words)
 
     def load(self):
         for i, line in enumerate(self.lines):
-            line = line.split('\t')[1]
-            words = line.split(" ")
+            ls = line.split('\t')
+            # for sentence in ls:
+            words = ls[0].split(" ")
             for j, word in enumerate(words):
-                self.text_array[i, j] = self.word_to_id[word.strip()]
+                self.x_array[i, j] = self.word_to_id[word.strip()]
+            words = ls[1].split(" ")
+            for j, word in enumerate(words):
+                self.y_array[i, j] = self.word_to_id[word.strip()]
 
-    def train_batchs(self, batch_nub):
+    def train_data(self, batch_nub):
         if self.iternub > self.max_iter:
             return None
         for _ in range(batch_nub):
             if self.train_batch_index > self.train_test_index:
                 self.train_batch_index = 0
                 self.iternub += 1
-            yield self.text_array[self.train_batch_index: (self.train_batch_index + self.batch_size)]
+            yield {
+                    'x_data': self.x_array[self.train_batch_index: (self.train_batch_index + self.batch_size)],
+                    'y_data': self.y_array[self.train_batch_index: (self.train_batch_index + self.batch_size)]
+                   }
             self.train_batch_index += self.batch_size
 
-    def test_batchs(self, batch_nub):
-        if self.iternub > self.max_iter:
-            return None
-        for _ in range(batch_nub):
-            if self.test_batch_index > len(self.lines):
-                self.test_batch_index = self.train_test_index
-                self.iternub += 1
-            yield self.text_array[self.test_batch_index: (self.test_batch_index + self.batch_size)]
-            self.test_batch_index += self.batch_size
+    def test_data(self):
+        yield{
+                'x_data': self.x_array[self.train_test_index:],
+                'y_data': self.y_array[self.train_test_index:]
+               }
+
+# todo x_data 应该增加一个维度表示对话 最长考虑5句对话？？？？填充？？？还是一个list？？？？
--- src/scripts/json_2_vocbulary_list.py
+++ src/scripts/json_2_vocbulary_list.py
@@ -5,25 +5,40 @@ import jieba
 
 json_path = r'../../corpus/dialog_datas/dialog.json'
 sentence_voc = r'../../corpus/dialog_datas/sentence_vocbulart.txt'
+sentence_dialog = r'../../corpus/dialog_datas/sentence_dialog.txt'
+
 sentences = set()
+dialog_set = set()
+dialog_set.add('null')
+
 with open(json_path, 'r') as json_file, \
-        open(sentence_voc, 'w', encoding='utf-8') as sentence_file:
+        open(sentence_voc, 'w', encoding='utf-8') as sentence_file, \
+        open(sentence_dialog, 'w', encoding='utf-8') as dialog_file:
     for line in json_file.readlines():
         line = line.strip()
         try:
             dialogs = json.loads(line)
-            # print(dialogs)
+            dialog_line = ''
             for sentence in dialogs['dialogs']:
+                sentence = sentence.replace('\t', '')
                 if sentence not in sentences:
                     sentences.add(sentence)
                     # sentence = sentence.encode('utf-8').decode('unicode_escape')
                     sentence_file.write(' '.join(jieba.cut(sentence))+'\n')
+                if '该评论已删除' in sentence:
+                    dialog_line = 'null'
+                    break
+                dialog_line += ((' '.join(jieba.cut(sentence))) + '\t')
+
+            if dialog_line not in dialog_set:
+                dialog_set.add(dialog_line)
+                dialog_file.write(dialog_line + '\n')
+                dialog_line = ''
         except Exception as e:
             print(e)
             print(line)
             pass
 
-# todo 处理 “此评论已删除”并统计这类语料
 
-
+# todo 这里应该包含各个层级的对话为一行
 
