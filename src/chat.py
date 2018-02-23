0be1e35f3e8d44ac0642b1ebdc42209f47f016af
hhxx2015 <zhenyuan_hao@163.com>
2018-02-23 16:00:11 +0800

    chat

--- src/chat.py
+++ src/chat.py
@@ -7,8 +7,8 @@ import jieba
 from src import data_load
 from src.model import ChatModel
 
-model_dir = r'..\model'
-content_path = r'..\corpus\dialog_datas\mini.sentence_dialog.txt'
+model_dir = r'..\model.0.01'
+content_path = r'..\corpus\dialog_datas\sentence_dialog.txt'
 voc_path = r'..\corpus\dialog_datas\voc'
 num_word = 26102
 embedding_dim = 128
@@ -19,7 +19,6 @@ decoder_rnn_state_size = 100
 attention_num_units = 100
 attention_depth = 100
 beam_width = 5
-learning_rate = 0.01
 
 batch_data = data_load.DataLoader(content_path=content_path, voc_path=voc_path)
 word_to_id = batch_data.word_to_id
@@ -42,11 +41,16 @@ def load_model():
             if x_str == "exit":
                 break
             feed_dict = build_dict(x_str=x_str, chat_model=chat_model)
-            beam_decoder_result_ids_, beam_decoder_sequence_outputs_ = \
-                sess.run([decoder_results['beam_decoder_result_ids'],
-                          decoder_results['beam_decoder_sequence_outputs']],
+            decoder_outputs_, beam_decoder_result_ids_, beam_decoder_sequence_outputs_,  = \
+                sess.run([
+                          decoder_results['decoder_outputs'],
+                          decoder_results['beam_decoder_result_ids'],
+                          decoder_results['beam_decoder_sequence_outputs'],
+
+                          ],
                          feed_dict)
-            out = [id_to_word[wid] for wid in beam_decoder_result_ids_[0][0]]
+            out = [id_to_word[wids[3]] for wids in beam_decoder_result_ids_[0]]
+            print(decoder_outputs_)
             print(out)
             print(beam_decoder_result_ids_)
             print(beam_decoder_sequence_outputs_)
